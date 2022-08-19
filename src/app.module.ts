e47bab30f8f3726d23521ff28456698716112006
Magno Carvalho <magno.carvalho@dorotech.com.br>
2022-08-19 04:13:42 -0300

    fix: nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClienteModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
